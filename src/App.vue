c2342cfb232fdecb75c0205cf182a12ba1311099
adelinashahaj <adelinarucaj@icloud.com>
2023-04-11 11:26:30 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <PageHeader/>
+  <PageMain/>
+  <PageFooter/>
+  
+</template>
+
+<script>
+import PageHeader from './components/PageHeader.vue';
+import PageMain from './components/PageMain.vue';
+import PageFooter from './components/PageFooter.vue';
+export default{
+  components: {
+    PageHeader,
+    PageMain,
+    PageFooter
+    
+  },
+    data(){
+        
+    }
+}
+
+</script>
+
+<style >
+
+</style>
